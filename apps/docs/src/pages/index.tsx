3b19bbbea4ba0ad235eaa57da8a16df815591108
laodeaksarr <akhsarodhe@gmail.com>
2022-10-10 05:20:06 +0000

    initial commit

---
+++ apps/docs/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@bahutara/core";
+
+export default function Docs() {
+  return (
+    <div>
+      <h1>bahutara Documentation</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
